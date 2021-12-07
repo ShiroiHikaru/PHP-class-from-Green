3ffc967242c52ef0c9c656eece24de780dc1c37b
ShiroiHikaru <asd95121@naver.com>
2021-12-07 11:23:08 +0900

    git add .

--- login/login.php
+++ login/login.php
@@ -19,19 +19,29 @@
             <p>
                 <label for="u_id">아이디</label>
                 <input type="text" name="u_id" id="u_id" class="u_id">
+                <span class="err_id"></span>
             </p>
             <p>
                 <label for="pwd">비밀번호</label>
                 <input type="password" name="pwd" id="pwd" class="pwd">
             </p>
             <p class="forget">
-                <button type="button" name="join" id="join" class="join">회원가입</button>
-                <button type="button" name="search" id="search" class="search">아이디/비밀번호 찾기</button>
-            </p>
-            <p>
-                <button type="" name="key" id="key" class="key">로그인하기</button>
+                <button type="button" name="join" id="join" class="join">이전으로</button>
+                <button type="button" name="key" id="key" class="key" onsubmit="return login_check()">로그인</button>
             </p>
         </fieldset>
     </form>
 </body>
+
+<script type="text/javascript">
+    function login() {
+        var uid = document.getElementById("u_id")
+        if(uid.value == ""){
+                var err_txt = document.querySelector(".err_id");
+                err_txt.textContent = "아이디를 입력하세요.";
+                uid.focus();
+                return false;
+        };
+    }
+</script>
 </html>
